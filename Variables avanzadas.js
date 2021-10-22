41d29113cda6777ae33fb68d5171ada2a930ed32
Mercedes Vazquez Alvarez <91942178+Mervazal@users.noreply.github.com>
2021-10-22 20:57:16 +0200

    Variables avanzadas.js
    Commit 1.3

--- Variables avanzadas.js
+++ Variables avanzadas.js
@@ -7,6 +7,11 @@ let lastName = 'Snow';
 let age = 24;
 console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos.`);
 
+const toy1 = {name: "Buss myYear", price: 19};
+const toy2 = {name: "Rallo mcKing", price: 29};
+console.log(toy1.price + toy2.price);
+
+
 
 
 
